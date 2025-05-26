1f8e8e2a2650a42125f17acd4c33c6302b68e02a
daryl <daryl@10.0.0.10>
2025-05-26 11:56:37 +0800

    feat: exit

--- apps/ui/src/views/Header/index.tsx
+++ apps/ui/src/views/Header/index.tsx
@@ -61,8 +61,8 @@ export const AppHeader: React.FC = () => {
         message={
           <div style={{ textAlign: 'center' }}>
             <Typography.Text>
-              Thank you for your continued support. To align with the evolving landscape of CKB ecosystem, <strong>Force
-                Bridge will enter the phase-out stage on June 1, 2025</strong>, and{' '}
+              Thank you for your continued support. To align with the evolving landscape of CKB ecosystem,{' '}
+              <strong>Force Bridge will enter the phase-out stage on June 1, 2025</strong>, and{' '}
               <strong>officially cease operation on November 30, 2025</strong>. For more detail, please refer to{' '}
               <Typography.Link href="https://sunset.forcebridge.com" target="_blank">
                 End of an Era: Force Bridge Sunset
